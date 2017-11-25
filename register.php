5d96996079c30c6f35ca06ff44fc8f7b459dcb7d
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-11-25 16:45:00 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,21 +1,41 @@
 <?php
-include 'connect.php';
+    require("password.php");
+   	include 'connect.php';
+    
 
-$username = $_POST["Username"];
-$email = $_POST["Email"];
-$password = $_POST["Password"];
+    $username = $_POST["Username"];
+	$email = $_POST["Email"];
+    $password = $_POST["Password"];
+	$passwordConfirm = $_POST["PasswordConfirm"];
 
-$statement = mysqli_prepare($conn,"INSERT INTO Users (Username,Email,Password) VALUES (?,?,?)");
-
-mysqli_stmt_bind_param($statement,"sss",$username,$email,$password);
-mysqli_stmt_execute($statement);
-
-$response = array();
-$response["success"] = true;
-
-echo json_encode($response);
-
-
-
-
+     function registerUser() {
+        global $conn, $username,$email, $password;
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $statement = mysqli_prepare($conn, "INSERT INTO Users (Username, Email, Password) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($statement, "sss", $username, $email, $passwordHash);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_close($statement);     
+    }
+    function usernameAvailable() {
+        global $conn, $username;
+        $statement = mysqli_prepare($conn, "SELECT * FROM Users WHERE Username = ?"); 
+        mysqli_stmt_bind_param($statement, "s", $username);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement); 
+        if ($count < 1){
+            return true; 
+        }else {
+            return false; 
+        }
+    }
+    $response = array();
+    $response["success"] = false;  
+    if (usernameAvailable() && $password == $passwordConfirm){
+        registerUser();
+        $response["success"] = true;  
+    }
+    
+    echo json_encode($response);
 ?>
